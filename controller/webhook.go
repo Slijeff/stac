0ceb768fd35d755ffc34605db83f9a51c6f263f5
Jeffrey Hui <jeff83636tx@gmail.com>
2023-04-27 22:31:55 -0500

    feat: parse event to struct

--- controller/webhook.go
+++ controller/webhook.go
@@ -1,19 +1,28 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"stac/parser"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/go-github/v52/github"
 )
 
 func HandleGithubWebhook(c *gin.Context) {
-	hook, err := parser.Parse([]byte("secret!"), c.Request)
+	// hook, err := parser.Parse([]byte("safsdfasdf"), c.Request)
+	hook, err := parser.ParseWithoutSecret(c.Request)
 	if err != nil {
 		fmt.Println("error: ", err)
+		return
 	}
-	fmt.Println(hook)
-	c.Header("test", "success!")
+	evt := github.PushEvent{}
+	err = json.Unmarshal(hook.Payload, &evt)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+	fmt.Println(evt.GetRepo().GetCloneURL())
 	c.JSON(200, gin.H{"copythat!": c.GetHeader("token")})
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,16 @@ go 1.20
 require github.com/gin-gonic/gin v1.9.0
 
 require (
+	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
+	github.com/cloudflare/circl v1.1.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-github/v52 v52.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
+
+require (
 	github.com/bytedance/sonic v1.8.8 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
